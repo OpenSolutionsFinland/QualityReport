8d3f0b7643d45961b48611bac94fde49fed889f9
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-10-15 09:21:08 +0300

    implementing quality report

--- stock_quality_report.py
+++ stock_quality_report.py
@@ -11,7 +11,7 @@ class stock_quality_report(report_sxw.rml_parse):
         })
 
 report_sxw.report_sxw(
-    'report.stock.move.quality',
+    'stock.move.quality',
     'stock.move',
     'addons/stock_quality_report/stock_quality_report.rml',
     parser=stock_quality_report
